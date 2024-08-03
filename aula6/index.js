d5ad379f371442ca844626b87c105d557587e29c
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-03 15:09:57 -0300

    Aula 6

--- aula6/index.js
+++ aula6/index.js
@@ -1,7 +1,16 @@
-let nome = 'João';
+let nome = 'Joaquim'; // Variável declarada com valor
+let data; // Variável declarada sem valor
+data = 1984; // Inicializando a variável
+console.log(nome, 'nasceu em', data);
+console.log('em 2000', nome, 'conheceu Maria.');
+data = 2012; // Modificando o valor da variável
+console.log(nome, 'casou-se com Maria em', data);
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
 
-console.log('João nasceu em 1984');
-console.log('em 2000 João conheceu Maria.');
-console.log('João casou-se com Maria em 2012');
-console.log('Maria teve 1 filho com João em 2015');
-console.log('O filho de João se chama Eduardo');
+// Não podemos criar variáveis com palavras reservadas
+// Exemplo: let let (let é uma palavra reservada para a linguagem)
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Nome das variáveis não podem conter espaços ou traços
+// Variáveis com palavras compostas usamos camelCase

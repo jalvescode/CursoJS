d9793a1e554ed42ad97bbe90f92eab125819e82d
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-04 14:53:44 -0300

    Aula 11

---
+++ aula11/index.js
@@ -0,0 +1,32 @@
+/* 
+
+ * Aritméticos + - / *
+  ** -> Potenciação
+  % Resto da divisão
+  ++ Incrementar 1 o valor da própria variável
+  -- Subtrair 1 o valor da própria variável
+  += Incrementa o valor de alguma variável ou valor
+ Ordem de precedência: 
+ () 
+ ** 
+ * / % 
+ + -
+*/
+/* 
+    Conversão de dados
+    parseInt (conversão para inteiro)
+    parseFloat (conversão para números flutuantes)
+    Number (conversão para número)
+
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+
+console.log(num1 + num2 * num3);
+console.log ((num1+num2)*num3);
+
+let incremento = 2;
+incremento++
+console.log(incremento)

813a3e7070f3c22c513d09d6ff70e64470e9fe31
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-04 21:54:43 -0300

    Aula 11

---
+++ aula11/js/script.js
@@ -0,0 +1,9 @@
+
+let num1 = prompt('Digite o primeiro numero');
+let num2 = prompt('Digite o segundo número');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(resultado)
+// Função dentro de um objeto = método
+// Função fora de um objeto = função

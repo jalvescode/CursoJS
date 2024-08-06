ad8041b67eebd8fa93a487cced17df083754f7c0
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-06 00:04:07 -0300

    Novas aulas

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -3,7 +3,6 @@ let num1 = prompt('Digite o primeiro numero');
 let num2 = prompt('Digite o segundo número');
 num1 = Number(num1);
 num2 = Number(num2);
-const resultado = num1 + num2;
-alert(resultado)
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
 // Função dentro de um objeto = método
 // Função fora de um objeto = função
---
+++ aula13/aula13.js
@@ -0,0 +1,7 @@
+let umaString = 'Um texto';
+
+console.log(umaString[4]) // [] pega o elemento da ordem indicada de uma string
+console.log(umaString.search('t')); // procura o index do valor indicado de uma string
+console.log(umaString.length) // Mostra o tamanho de uma string
+console.log(umaString.slice(3,9)) // Fatiar um tamanho especifico de uma string
+console.log(umaString.split(' ')) // Dividir cada indíce pelo valor indicado

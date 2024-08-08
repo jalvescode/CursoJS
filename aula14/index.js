bfd9fe0dd76404618df89200d574e8d6397cea0a
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-07 21:31:38 -0300

    Aulas novas

---
+++ aula14/index.js
@@ -0,0 +1,13 @@
+let num1 = 1; // number
+let num2 = 2.5; // number
+let num3 = 10.32301231293193
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.tostring(2)) -> Transforma o número em número binário equivalente
+
+console.log(num3.toFixed(2)); // Especifico quantas casas decimais o valor será fixado
+
+console.log(Number.isInteger(num1)); // Verificar se o número é inteiro
+let temp = num1 * 'Ola';
+console.log(Number.isNaN(temp)); // Verifico se a conta é um número invalido
---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 25;
+console.log(num1 ** 0.5);
+
+
+
+// let num2 = Math.floor(num1); -> ARREDONDA PARA BAIXO O NÚMERO
+// let num2 = Math.ceil(num1); -> ARREDONDA PARA CIMA
+// let num2 = Math.round(num1); -> ARREDONDA PARA O NÚMERO INTEIRO MAIS PRÓXIMO
+// console.log(Math.max(1,2,3,4,5,6,7,2313213,232,13)); -> MAIOR NÚMERO DA SEQUÊNCIA
+// console.log(Math.min(1,2,3,4,5,6,7,2313213,232,13)); -> MENOS NÚMERO DA SEQUÊNCIA
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);

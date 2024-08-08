8dbfbeda40da46b655590c5b014236b0f1fe019c
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-08 20:29:09 -0300

    Aula 17

---
+++ aula17/index.js
@@ -0,0 +1,35 @@
+// indice     012345678910  
+const nome = 'Luiz Otávio'
+console.log(nome[0])
+
+// indice         0         1       2
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+console.log(typeof alunos);
+
+// console.log(alunos.slice(0, 2)); // Fatiar uma quantidade de arrays
+
+// delete alunos[1]; -> Deleta o valor da array especificada
+// console.log(alunos);
+
+// alunos.pop(); -> Remove a ultima array
+
+// alunos.shift(); -> Remove a primeira array
+
+// alunos.unshift('Luiza'); -> Adiciona ao inicio da array
+
+
+
+// alunos.push('Otávio'); -> Adiciona no fim da array
+// alunos.push('Luiza');
+// alunos.push('Fábio');
+
+
+
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; -> Altera uma array já existente
+// alunos[3] = 'Luiza'; -> Adiciona uma nova array

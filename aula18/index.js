dd733e5ccff9d933f45273f9b136304aac47c3ae
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-12 21:13:22 -0300

    Create index.js

---
+++ aula18/index.js
@@ -0,0 +1,10 @@
+/* function soma(x = 1, y = 1) {
+    const resultado = x+y
+    return resultado;
+}
+
+console.log(soma(4)) */
+
+const raiz = n => n ** 0.5; // Arrow functions
+
+console.log(raiz(9))

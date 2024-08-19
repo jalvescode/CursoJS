3ebbb82874b97c4de76fa1f3a6e19c3134f3ef02
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-08-18 23:05:44 -0300

    Aulas

---
+++ Operadores de comparação/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== Igualdade (valor)
+=== Igualdade estrita (valor e tipo)
+!= Diferente (valor)
+!== Diferente estrito (valor e tipo)
+
+*/
+const expressao = 10 > 5;
+console.log(typeof expressao);
---
+++ Operadores lógicos/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> Alguma expressão precisa ser verdadeira para retornar true
+! -> NOT -> 
+*/
+
+//const expressaoAnd = true && true && true;
+//const expressaoOr = true || false;
+//console.log(expressaoAnd)
+//console.log(expressaoOr)
+
+/* const usuario = 'Luiz';
+const senha = '12345';
+//                            True                False
+const vaiLogar = usuario === 'Luiz' && senha === '12345';
+console.log(vaiLogar);
+*/
+
+console.log(!true);

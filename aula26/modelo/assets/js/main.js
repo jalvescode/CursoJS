9647613e835e54046c7fbf79b7d58049b799539a
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-09-12 22:59:56 -0300

    Aula 26

--- aula26/modelo/assets/js/main.js
+++ aula26/modelo/assets/js/main.js
@@ -7,9 +7,22 @@ function meuEscopo (){
         let altura = form.querySelector('.altura')
         peso = peso.value;
         altura = altura.value;
-        let resultadoIMC = 0;
-        resultadoIMC = peso*altura/altura;
-        resultado.innerHTML = `<p>${resultadoIMC}</p>`;
+        let resultadoIMC;
+        resultadoIMC = peso / (altura*altura);
+        if (resultadoIMC <= 18.5) {
+            resultado.innerHTML = `<p>Abaixo do peso</p>`;
+        } else if (resultadoIMC >= 18.5 && resultadoIMC <= 24.9){
+            resultado.innerHTML = `<p>Peso Normal</p>`
+        } else if (resultadoIMC >= 25 && resultadoIMC <=29.9){
+            resultado.innerHTML = `<p>Sobrepeso</p>`
+        } else if (resultadoIMC >= 30 && resultadoIMC <=34.9){
+            resultado.innerHTML = `<p>Obesidade grau 1</p>`
+        } else if (resultadoIMC >= 35 && resultadoIMC <=39.9){
+            resultado.innerHTML = `<p>Obesidade grau 2</p>`
+        } else if (resultadoIMC >= 40){
+            resultado.innerHTML = `<p>Obesidade grau 3</p>`;
+        }
+
     }
     form.addEventListener('submit', calculoImc)
 }

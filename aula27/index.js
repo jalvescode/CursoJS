84d3ca8b5e49929f0223be716d714ecd8da7594d
jalvescode <155323562+jalvescode@users.noreply.github.com>
2024-09-15 20:48:44 -0300

    Aula 27

---
+++ aula27/index.js
@@ -0,0 +1,6 @@
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso'
+
+const pontuacaoUsuario = 100;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'VIP' : 'Normal';
+
+console.log(nivelUsuario)
